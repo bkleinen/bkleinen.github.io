c3d003952a3f9141a1cce9bb8cac34d05627f926
B Kleinen <kleinen@infrastructure.de>
2014-03-28 12:28:18 +0100

    aliasses for ss2014

--- aliasses.sh
+++ aliasses.sh
@@ -1,6 +1,7 @@
 alias os='open http://localhost:4000'
-alias i1='open http://localhost:4000/ws2013/info1'
-alias i3='open http://localhost:4000/ws2013/info3'
+alias i1='open http://localhost:4000/ss2014/info1'
+alias i2='open http://localhost:4000/ss2014/info2'
 alias htw='open http://people.f4.htw-berlin.de/people/kleinen/'
 alias gh='open http://bkleinen.github.io'
 alias rj='jekyll serve -w'
+cat aliasses.sh
