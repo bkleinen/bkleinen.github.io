14b6f7f396d8736ba04fe56dd743fdcac42f54ff
B Kleinen <kleinen@infrastructure.de>
2019-09-26 17:32:30 +0200

    still a race condition, but in better favor for going right

--- _bin/check_versions.sh
+++ _bin/check_versions.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 wget -O commit-htw.txt http://home.htw-berlin.de/~kleinen/commit.txt
 VERSION_HTW=$(cat commit-htw.txt)
-VERSION_GITHUB=$(git log --pretty=format:'%h' -n 1)
-if [ $VERSION_HTW == $VERSION_GITHUB ]; then
+VERSION_GIT=$(git log --pretty=format:'%h' -n 1)
+if [ $VERSION_HTW == $VERSION_GIT ]; then
   echo "versions match"
   exit 0
 else
--- _bin/deploy-htw.sh
+++ _bin/deploy-htw.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 git log --pretty=format:'%h' -n 1 > commit.txt
-git push origin master
-if [ $? -ne 0 ]; then
-    echo "push failed; exiting"
-    exit $?
-fi
 
 jekyll build --config _config.yml,_htwconfigrz.yml
 
@@ -13,7 +8,14 @@ if [ $? -eq 0 ]; then
 #    for f in $(find _site_htw_rz -name "*.html"); do iconv -f utf-8 -t ISO-8859-1 $f > temp.html && mv -f temp.html $f; done
     scp -r _site_htw_rz/* kleinen@remserv.rz.htw-berlin.de:/home/user/K/kleinen/public_html
 else
-    echo SITE BUILD FAILED, NOT COPYING TO HTW
+    echo "SITE BUILD FAILED, NOT COPYING TO HTW"
+    exit $?
+fi
+
+git push origin master
+if [ $? -ne 0 ]; then
+    echo "push failed; exiting"
+    exit $?
 fi
 
 # if iconv fails, or gsub in HTMLProofer fails, check for invalid UTF-8
