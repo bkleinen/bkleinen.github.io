447ada182c442e8d48ddbb1a88a2d127298d70ce
B Kleinen <kleinen@infrastructure.de>
2020-06-09 12:12:47 +0200

    info2

--- _bin/ruby-dates.rb
+++ _bin/ruby-dates.rb
@@ -12,19 +12,29 @@ require_relative './lib_ruby_dates.rb'
 # )
 # WT4
 course_dates = CourseDates.new(
-  first_day: Date.parse("1.4.2020"),
+  first_day: Date.parse("1.10.2020"),
   day_diff: 2,
   two_lectures: false
 )
 
 # Semesterdaten
 semester_dates = SemesterDates.new(
-semester_start: Date.parse("1.4.2020"),
-semester_end: Date.parse("18.07.2020"),
-holidays: {"11.04.2020" => "Karfreitag",
-  "13.04.2020" => "Ostermontag",
-  "21.05.2020" => "Himmelfahrt",
-  "22.05.2020" => "Brückentag nach Himmelfahrt"},
+semester_start: Date.parse("1.10.2020"),
+semester_end: Date.parse("13.02.2021"),
+holidays: {
+  "21.12.2020" => "Vorlesungsfrei",
+  "22.12.2020" => "Vorlesungsfrei",
+  "23.12.2020" => "Vorlesungsfrei",
+  "24.12.2020" => "Vorlesungsfrei",
+  "25.12.2020" => "Vorlesungsfrei",
+  "26.12.2020" => "Vorlesungsfrei",
+  "27.12.2020" => "Vorlesungsfrei",
+  "28.12.2020" => "Vorlesungsfrei",
+  "29.12.2020" => "Vorlesungsfrei",
+  "30.12.2020" => "Vorlesungsfrei",
+  "31.12.2020" => "Vorlesungsfrei",
+  "1.1.2021" => "Vorlesungsfrei",
+  "2.1.2021" => "Vorlesungsfrei"},
 reject_span:[]
 )
 
