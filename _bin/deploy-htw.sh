603fc0ef98a7354cc5edd4a61ae94392b360f860
B Kleinen <kleinen@infrastructure.de>
2020-09-29 15:12:23 +0200

    updated hrz settings

--- _bin/deploy-htw.sh
+++ _bin/deploy-htw.sh
@@ -13,7 +13,7 @@ jekyll build --config _config.yml,_htwconfigrz.yml
 if [ $? -eq 0 ]; then
 # locale charmap on server says US-ASCII, so not necessary for this server
 #    for f in $(find _site_htw_rz -name "*.html"); do iconv -f utf-8 -t ISO-8859-1 $f > temp.html && mv -f temp.html $f; done
-    scp -r _site_htw_rz/* kleinen@remserv.rz.htw-berlin.de:/home/user/K/kleinen/public_html
+    scp -r _site_htw_rz/* oxid01.rz.htw-berlin.de:/home/user/K/kleinen/public_html
 else
     echo "SITE BUILD FAILED, NOT COPYING TO HTW"
     exit $?
