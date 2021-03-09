b620495334511a98d868a00ecfb631e3bf9d01f9
Barne Kleinen <barne.kleinen@htw-berlin.de>
2021-03-09 13:51:26 +0100

    update aliasses

--- aliasses.sh
+++ aliasses.sh
@@ -1,8 +1,8 @@
 alias os='open http://localhost:4000'
-alias oc='open http://localhost:4000/classes/ws2020/'
-alias i3='open http://localhost:4000/classes/ws2020/info3/'
+alias ot='open http://localhost:4000/classes/ss2021/'
+alias i1='open http://localhost:4000/classes/ss2021/info1/'
 alias wt='open http://localhost:4000/classes/ws2020/m-wt1-webapplications'
-alias m1='open http://localhost:4000/classes/ws2020/m1-web'
+alias m1='open http://localhost:4000/classes/ss2021/m1-web'
 alias htw='open https://home.htw-berlin.de/~kleinen/'
 alias gh='open https://bkleinen.github.io'
 # see https://jekyllrb.com/docs/drafts/
