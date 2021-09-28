39436df816e9f2b6d9a7d1b4ce4145173b0c3cf9
B Kleinen <kleinen@infrastructure.de>
2021-09-28 16:13:30 +0200

    split coursenav formats

--- _bin/hugo_migration/migrate_classes.rb
+++ _bin/hugo_migration/migrate_classes.rb
@@ -1,5 +1,36 @@
 require 'yaml'
 
+def extractInt(coursenav)
+
+  r2 = /{% assign navitems = \"([^ ]*)" | split: "," %}/
+  m =  r2.match(coursenav)
+  if m
+    navitems = m[1]
+    puts navitems
+    menu = navitems.split(",").map{|n| n = n.split("-") ; { "title" => n[0], "link" => n[1]} }
+  else
+    puts "----- no match found!"
+    puts coursenav
+    {}
+  end
+end
+
+def extractExt(coursenav)
+
+    r3 = /<a class="dropdown-item" target = "ex_link" href="([^"]*)">(.*)<\/a>/
+    menu = coursenav.scan(r3).map{|n| { "title" => n[0], "link" => n[1]} }
+    {}
+end
+
+def extractNewFormat(coursenav)
+  {}
+end
+
+def extractOldFormat(coursenav)
+  r4 = /\({{ site.baseurl }}(.*)\)/
+  {}
+end
+
 Dir.glob("classes/*/*/index.md")
 
 # read the list of available coursenavigations from the old coursenav
@@ -14,22 +45,14 @@ puts defined_coursenavs.inspect
 
 defined_coursenavs.each do | coursenav_include |
   coursenav = File.open("_includes/"+coursenav_include).read
+  # decide if old or new format
+  r2 = /{% assign navitems = \"([^ ]*)" | split: "," %}/
+  format = r2.match(coursenav) ? :new : :old
   puts "-------------------------------------------"
   puts "coursenav_include: "+coursenav_include
-  r2 = /{% assign navitems = \"([^ ]*)" | split: "," %}/
-  m =  r2.match(coursenav)
-  if m
-    navitems = m[1]
-    puts navitems
-    menu = navitems.split(",")
-    m
-    Overview-/classes/ws2021/networks/,Schedule-/classes/ws2021/networks/schedule/,Resources-/classes/ws2021/networks/resources/,Labs-/classes/ws2021/networks/labs/
-    puts YAML.dump("foo")
-
-  else
-    puts "----- no match found!"
-    puts coursenav
-  end
+  puts "format: #{format}"
+  extracted = (format == :old) ? extractOldFormat(coursenav) : extractNewFormat(coursenav)
+  puts YAML.dump(extracted)
 
 
 end
