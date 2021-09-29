5fe4f9ed2bf7d8a37c8b6f9971756e8b61085853
B Kleinen <kleinen@infrastructure.de>
2021-09-29 19:01:17 +0200

    exit on error

--- _bin/hugo_deployment/steps.sh
+++ _bin/hugo_deployment/steps.sh
@@ -1,9 +1,22 @@
+#!/bin/bash
+exit_on_error () {
+  sshexit=$1
+  if [ $sshexit -eq 0 ]; then
+    echo "step ok"
+  else
+      echo "ERROR: step failed with exit code $sshexit"
+      echo "ERROR: deployment failed"
+      exit $sshexit
+  fi
+}
 eval $(ssh-agent -s)
-ls -lart 
 ssh-add ~/.tmp
+exit_on_error $?
 ssh-add -L
 # rm ~/.tmp
 date > ./hugo-tunnel/public-htw/timestamp
 ssh -o StrictHostKeyChecking=no $HOST2 "mkdir -p /home/user/K/kleinen/public_html"
+exit_on_error $?
 scp -r -o StrictHostKeyChecking=no ./hugo-tunnel/public-htw/* $HOST2:/home/user/K/kleinen/public_html
+exit_on_error $?
 eval "$(ssh-agent -k)"
