a7a7677421ee426c6a675a76e58f6888c7d78d83
B Kleinen <kleinen@infrastructure.de>
2021-09-29 14:18:28 +0200

    deploy real site with hugo

--- _bin/hugo_deployment/steps.sh
+++ _bin/hugo_deployment/steps.sh
@@ -1,6 +1,6 @@
 eval $(ssh-agent -s)
 ssh-add ~/.tmp
 rm ~/.tmp
-ssh -o StrictHostKeyChecking=no $HOST2 "mkdir /home/user/K/kleinen/public_html/hugo"
-scp -r -o StrictHostKeyChecking=no ./hugo-tunnel/* $HOST2:/home/user/K/kleinen/public_html/hugo
+ssh -o StrictHostKeyChecking=no $HOST2 "mkdir -p /home/user/K/kleinen/public_html"
+scp -r -o StrictHostKeyChecking=no ./hugo-tunnel/* $HOST2:/home/user/K/kleinen/public_html
 eval "$(ssh-agent -k)"
