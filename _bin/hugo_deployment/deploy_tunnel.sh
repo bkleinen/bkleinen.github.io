2d17d9022da6f0b2a2fdee56b3535109d72d7817
B Kleinen <kleinen@infrastructure.de>
2021-09-29 15:06:13 +0200

    rel url

--- _bin/hugo_deployment/deploy_tunnel.sh
+++ _bin/hugo_deployment/deploy_tunnel.sh
@@ -1,5 +1,5 @@
 
-  hugo  --source hugo --destination ../public-htw --environment htw --baseURL https://home.htw-berlin.de/~kleinen/hugo
+  hugo  --source hugo --destination ../public-htw --environment htw 
 
   ssh -o StrictHostKeyChecking=no $HOST1 "mkdir -p ~/hugo-tunnel"
   scp -r -o StrictHostKeyChecking=no  ./public-htw/* $HOST1:~/hugo-tunnel > /dev/null
