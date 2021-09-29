5f30779f709fb5ba841755722db5fb038ca8451d
B Kleinen <kleinen@infrastructure.de>
2021-09-29 16:29:57 +0200

    prod deployment

--- _bin/hugo_deployment/steps.sh
+++ _bin/hugo_deployment/steps.sh
@@ -1,6 +1,8 @@
 eval $(ssh-agent -s)
 ssh-add ~/.tmp
-rm ~/.tmp
+ssh-add -L
+# rm ~/.tmp
+date > ./hugo-tunnel/timestamp
 ssh -o StrictHostKeyChecking=no $HOST2 "mkdir -p /home/user/K/kleinen/public_html"
 scp -r -o StrictHostKeyChecking=no ./hugo-tunnel/* $HOST2:/home/user/K/kleinen/public_html
 eval "$(ssh-agent -k)"
