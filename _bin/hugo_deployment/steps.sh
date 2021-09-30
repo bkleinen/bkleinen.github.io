33a46463f388fc0499e9e421148df0bc639e6e32
B Kleinen <kleinen@infrastructure.de>
2021-09-30 23:05:58 +0200

    remove key fix

--- _bin/hugo_deployment/steps.sh
+++ _bin/hugo_deployment/steps.sh
@@ -9,19 +9,11 @@ exit_on_error () {
       exit $sshexit
   fi
 }
-sed -e "s/-----BEGIN OPENSSH PRIVATE KEY-----/###BEGIN###\n/" \
-    -e "s/-----END OPENSSH PRIVATE KEY-----/\n###END###/"  \
-    -e "s/ //g" \
-    -e "s/###BEGIN###/-----BEGIN OPENSSH PRIVATE KEY-----/" \
-    -e "s/###END###/-----END OPENSSH PRIVATE KEY-----/"  \
-     -e "s/.\{64\}/&\n/g" ./.tmp > .fixed
-chmod 600 .fixed
 eval $(ssh-agent -s)
-ssh-add ~/.fixed
+ssh-add ~/.tmp
 exit_on_error $?
 ssh-add -L
-# rm ~/.tmp
-# rm ~/.fixed
+rm ~/.tmp
 date > ./hugo-tunnel/public-htw/timestamp
 ssh -o StrictHostKeyChecking=no $HOST2 "mkdir -p /home/user/K/kleinen/public_html"
 exit_on_error $?
