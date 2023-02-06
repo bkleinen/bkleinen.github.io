1c086cb15f77fe405577f833df5e88a418822f25
Barne Kleinen <barne.kleinen@htw-berlin.de>
2023-02-06 01:05:17 +0100

    .

--- .github/linkcheck.sh
+++ .github/linkcheck.sh
@@ -3,4 +3,5 @@ HUGO_PID=$!
 sleep 1
 tail -f /tmp/hugo.output | timeout 120 grep -qe 'Press Ctrl+C to stop'
 echo "Hugo has started"
-docker run --rm tennox/linkcheck host.docker.internal:1313 &
+curl http://localhost:1313
+docker run --rm tennox/linkcheck localhost:1313 
